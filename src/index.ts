760fc75b1496469dbcbdc0c174b1ae9e4654c140
moteeullahazmi <moteeullahazmi@gmail.com>
2025-01-02 15:19:33 +0530

    42

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Azmi")
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+let countUser = 0;
+let allSocket: WebSocket[] = [];
+
+
+wss.on("connection", (socket) => {
+    countUser = countUser + 1;
+    allSocket.push(socket);
+
+    console.log(countUser)
+    socket.on('message', (message) => {
+
+        for (let i = 0; i < allSocket.length; i++) {
+            let s = allSocket[i]
+            s.send(message.toString() +  "sent from server")
+
+        }
+    })
+})
