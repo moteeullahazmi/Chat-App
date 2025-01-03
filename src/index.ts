0f2f6c030cf8b03f78ef87fae6389cad654eedde
moteeullahazmi <moteeullahazmi@gmail.com>
2025-01-03 16:07:54 +0530

    complete 124

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,40 @@
 import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
-let countUser = 0;
-let allSocket: WebSocket[] = [];
+
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+let allSockets: User[] = [];
 
 
 wss.on("connection", (socket) => {
-    countUser = countUser + 1;
-    allSocket.push(socket);
 
-    console.log(countUser)
-    socket.on('message', (message) => {
+    socket.on("message", (message) => {
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == "join") {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
 
-        for (let i = 0; i < allSocket.length; i++) {
-            let s = allSocket[i]
-            s.send(message.toString() +  "sent from server")
+        if (parsedMessage.type == "chat") {
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room
+                }
+            }
 
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     })
 })
